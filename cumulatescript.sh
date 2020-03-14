a55ad935d060c6f698b498fdbe135cef0c72c5e3
ncface <ncface@gmx.de>
2020-03-14 17:24:03 +0000

    added cumulatescript and gitignore to ignore data

---
+++ cumulatescript.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# create the output directory (if it does not exist)
+mkdir -p output
+# remove result from previous runs
+rm output/*.json
+# add first opening bracked
+echo { >> output/tmp.json
+# use all json files in current folder
+for i in *.json
+do 
+    # first create the key; it is the filename without the extension
+    echo \"$i\": | sed 's/\.json//' >> output/tmp.json
+    # dump the file's content
+    cat "$i" >> output/tmp.json
+    # add a comma afterwards
+    echo , >>  output/tmp.json
+done
+# remove the last comma from the file; otherwise it's not valid json
+cat output/tmp.json | sed '$ s/.$//' >> output/out.json
+# remove tempfile
+rm output/tmp.json
+# add closing bracket
+echo } >> output/out.json
--- script.sh
+++ script.sh
@@ -4,3 +4,4 @@
 #echo "This is a script, run by cron!"
 
 wget http://api.coronatracker.com/v2/analytics/country -O /data/corona_$(date +%Y-%m-%d_%H:%M:%S).json
+chmod 777 /data/*
